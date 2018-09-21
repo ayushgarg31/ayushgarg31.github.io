ce069337351f2bf2dfb7ab1a479158ad6b3286ee
Ayush Garg <40912846+ayushgarg31@users.noreply.github.com>
2018-09-22 03:26:42 +0530

    2 projects

--- js/index.js
+++ js/index.js
@@ -1,9 +1,7 @@
 function topbar() {
   if (window.pageYOffset >= window.innerHeight) {
-    navbar.classList.add("show");
     navbar.style.visibility="visible";
   } else {
   	navbar.style.visibility="hidden";
-  	navbar.classList.add("show");
   }
 }
